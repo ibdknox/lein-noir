f1dcfd9602e9de40dcf1059ec8d54e5ca07e9097
Chris Granger <ibdknox@gmail.com>
2011-06-02 17:38:33 -0700

    fix project file, inc version
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject lein-noir "1.0.0-SNAPSHOT"
-            :description "FIXME: write description"
+(defproject lein-noir "0.1.0"
+            :description "A leiningen plugin to create and manage noir projects"
             :dependencies [[org.clojure/clojure "1.2.1"]])
