5757580c2cabb151e40553bb2e7cbc2cb04a6406
Chris Granger <ibdknox@gmail.com>
2011-06-13 01:02:18 -0700

    inc version
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-noir "0.1.0"
+(defproject lein-noir "0.1.1"
             :description "A leiningen plugin to create and manage noir projects"
             :dependencies [[org.clojure/clojure "1.2.1"]]
             :dev-dependencies [[lein-clojars "0.6.0"]]
