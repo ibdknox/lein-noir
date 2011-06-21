25badea9405149e31bd031349e2dc513a619a4f8
Chris Granger <ibdknox@gmail.com>
2011-06-21 16:14:50 -0700

    adjust project template to use the latest noir, inc version
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
-(defproject lein-noir "0.1.1"
+(defproject lein-noir "1.0.0"
             :description "A leiningen plugin to create and manage noir projects"
-            :dependencies [[org.clojure/clojure "1.2.1"]]
-            :dev-dependencies [[lein-clojars "0.6.0"]]
-            )
+            :dependencies [[org.clojure/clojure "1.2.1"]])
