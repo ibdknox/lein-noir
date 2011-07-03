f29f8986008ab1694377d44a0bd8719b30caa139
Chris Granger <ibdknox@gmail.com>
2011-07-03 01:37:46 -0700

    move to latest noir
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- resources/templates/project.clj
+++ resources/templates/project.clj
@@ -1,6 +1,6 @@
 (defproject $project$ "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.2.1"]
-                           [noir "1.0.0"]]
+                           [noir "1.1.0-SNAPSHOT"]]
             :main $project$.server)
 
