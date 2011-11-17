1f919685d308407c45095829c179d2e572261a82
Anthony Grimes <i@raynes.me>
2011-11-17 00:15:30 -0600

    Use lein-newnew.

---
+++ src/leiningen/new/noir/common.clj
@@ -0,0 +1,12 @@
+(ns {{name}}.views.common
+  (:use [noir.core :only [defpartial]]
+        [hiccup.page-helpers :only [include-css html5]]))
+
+(defpartial layout [& content]
+            (html5
+              [:head
+               [:title "{{name}}"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                content]]))
--- test/lein_noir/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns lein-noir.test.core
-  (:use [lein-noir.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
