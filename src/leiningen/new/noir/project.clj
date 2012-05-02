8168e0168dc583075ce64d11e7041c26433c788f
Chris Granger <ibdknox@gmail.com>
2012-05-01 22:01:58 -0700

    default to 1.4.0
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/leiningen/new/noir/project.clj
+++ src/leiningen/new/noir/project.clj
@@ -1,6 +1,6 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.3.0"]
+            :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]]
             :main {{name}}.server)
 
